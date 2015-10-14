782b32d40f768aa554923fa0b7ad5710f8bb93d6
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-10-13 22:18:38 -0500

    GPLv2 -> GPLv3

--- common/error.h
+++ common/error.h
@@ -7,7 +7,7 @@
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
+ * Foundation; either version 3 of the License, or (at your option) any later
  * version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -54,6 +54,7 @@ int disco_check_args(int num, ...) {
   return 0;
 }
 
+/* Fails if no arguments are given. */
 #define DISCO_CHECK_ARGS(...)                                        \
   do {                                                               \
     if (disco_check_args(DISCO_PP_NARG(__VA_ARGS__), __VA_ARGS__)) { \
