0da7f23abc12d95923bd0ee6d7303700bedf38f2
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-10-19 02:32:56 -0500

    add lgpl

--- rng/rng.c
+++ rng/rng.c
@@ -1,3 +1,21 @@
+/**
+ * Random number generation for the DISCOTANGO library.
+ * Copyright (c) 2015 Danny McClanahan
+ *
+ * DISCOTANGO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DISCOTANGO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DISCOTANGO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <stdbool.h>
 #include "rng.h"
--- rng/rng.h
+++ rng/rng.h
@@ -1,6 +1,24 @@
 #ifndef __DISCO_RNG_H__
 #define __DISCO_RNG_H__
 
+/**
+ * Random number generation for the DISCOTANGO library.
+ * Copyright (c) 2015 Danny McClanahan
+ *
+ * DISCOTANGO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DISCOTANGO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DISCOTANGO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stddef.h>
 
 typedef int disco_rng_return_t;
