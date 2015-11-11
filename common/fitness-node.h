d753aa1727ff000efaeca8dccec81ab9e15a9e53
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-11-11 12:59:59 -0600

    make macro safer

--- common/fitness-node.h
+++ common/fitness-node.h
@@ -8,11 +8,11 @@ static int disco_check_byte __attribute__((unused)) = 1 / (sizeof(char) == 1);
                                    in_state, out_state, out_fit, out_arr,    \
                                    in_arr, stmts)                            \
   do {                                                                       \
-    for (size_t ind = 0; ind < arr_len; ++ind) {                             \
-      out_ind   = ((char *) out_arr) + (ind * struct_size);                  \
-      in_state  = (void *) ((char *) in_arr) + (ind * data_size);            \
-      out_state = out_ind + sizeof(double);                                  \
-      out_fit = (double *) out_ind;                                          \
+    for (size_t _____ind = 0; _____ind < (arr_len); ++_____ind) {            \
+      (out_ind) = ((char *) (out_arr)) + (_____ind * (struct_size));         \
+      (in_state) = (void *) ((char *) (in_arr)) + (_____ind * (data_size));  \
+      (out_state) = (out_ind) + sizeof(double);                              \
+      (out_fit) = (double *) (out_ind);                                      \
       do { stmts } while (0);                                                \
     }                                                                        \
   } while (0)
